08cdda04e188e77e120d5e1b6ecf5b54d8156c7d
Martin Akman <martin@akm.com.au>
2013-12-09 13:08:29 +1000

    Add skeleton public product controller

---
+++ controllers/Products.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace modules\products\controllers;
+
+use core\classes\exceptions\RedirectException;
+use core\classes\exceptions\SoftRedirectException;
+use core\classes\renderable\Controller;
+use core\classes\Model;
+use core\classes\Pagination;
+
+class Products extends Controller {
+
+	public function index() {
+
+	}
+
+}
